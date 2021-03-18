d26bef5d17099b89207178e3e28db2537e709b3c
Greg P <gplechaty@gmail.com>
2021-03-17 21:51:08 -0600

    player and team done, starting season

--- mlbstats/views.py
+++ mlbstats/views.py
@@ -7,49 +7,90 @@ import csv
 def homepage(request):
     print('--------view homepage')
     
-    bar_chart = pygal.Bar()
-    bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
-    b = bar_chart.render_data_uri()
+    
     
     context = {
         'adoptions_needed': 5,
-        'b': b, 
+ 
     }
     return render(request, 'homepage.html', context)
 
 def season(request):
     print('--------view season')
-    return render(request, 'season.html')
+    context = {
+        'adoptions_needed': 5,
+ 
+    }
+    return render(request, 'season.html', context)
 
 def team(request):
     print('--------view team')
-    return render(request, 'team.html')
+    ##read in file
+    file = 'mlbstats/static/team-year-detail-stats/brewers-2020.csv'
+    list_of_rows = []
+    with open(file) as csv.file:
+            csv_reader = csv.reader(csv.file, delimiter=',')
+            
+    #create header. format = list
+            header = next(csv_reader)
+    #Create each record. loop through .csv
+            for row in csv_reader:
+                row[2] = row[2].split('\\')[0]
+                print(row[2])
+                list_of_rows.append(row)
+            
+    #return lists. html will actually format
+    context = {
+        'header': header,
+        'rows': list_of_rows
+        
+    }
+    
+    return render(request, 'team.html', context)
 
 def player(request):
     print('--------view player')
     player_name = ''
     stat_line_chart = ''
-    if 'player' in request.GET:
-        print('player info request:', request.GET['player'])
-        player_name = request.GET['player']
-        file = 'mlbstats/static/player-stats/ryan-braun-player_stats.csv'
+    if 'player1' in request.GET:
+        print('player info request:', request.GET['player1'])
+        player_name = request.GET['player1']
+        stat_form = request.GET['stat'].upper()
+        print(stat_form)
+        file = 'mlbstats/static/player-stats/ryan-braun_player_stats.csv'
+        ####################
+        list_for_csv = []
         x_labels = []
+        y_values = []
+        ####################
         with open(file) as csv.file:
             csv_reader = csv.reader(csv.file, delimiter=',')
-            next(csv_reader)
+            header = next(csv_reader)
+            for idx, item in enumerate(header):
+                if item == stat_form:
+                    stat_csv_position = idx
+                    break
+            list_for_csv.append(csv_reader)
+            # find what column to read data from
+            #header = csv_reader[0]
+            #print(header)
+                #if item == statistic:
+                    #statistic_csv = 88
             for row in csv_reader:
                 x_labels.append(row[0])
-        print(x_labels)
-        print(x_labels[0])
-        print(len(x_labels))
+                y_values.append(float(row[idx]))
+            print
+            
+            print(idx)
+            print('y values:', y_values)
+        
+
         
         line_chart = pygal.Bar()
-        line_chart.title = 'RBIs per Season'
+        line_chart.title = stat_form + ' per Season'
         line_chart.x_labels = map(str, x_labels)
-        line_chart.add('Firefox', [None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+        line_chart.add('Ryan Braun', y_values)
         line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
-        line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-        line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
         stat_line_chart = line_chart.render_data_uri()
 
 
