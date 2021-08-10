cea84e9695fa5ac26bb1a7f6d026be5827ff1102
Greg P <gplechaty@gmail.com>
2021-08-10 17:15:12 -0600

    Player stats: two players output data, but still need to fill in 0 y values. gonna refactor next.

--- mlbstats/views.py
+++ mlbstats/views.py
@@ -90,28 +90,24 @@ def team(request):
 def player(request):
     print('--------view player')
     player_name1 = ''
+    player_name2 = ''
     stat_line_chart = ''
     if 'player1' in request.GET:
         print('---GET REQUEST:', request.GET)
         player_name1 = request.GET['player1']
+        player_name2 = request.GET['player2']
         stat_form = request.GET['stat'].upper()
-        file = 'mlbstats/static/player-stats/' + player_name1 + '_player_stats.csv'
-        x_labels, y_values = generate_chart_values(file, stat_form)
-        stat_line_chart = create_chart(stat_form, player_name1, x_labels, y_values)
+        file2 = None
+        file1 = 'mlbstats/static/player-stats/' + player_name1 + '_player_stats.csv'
+        if player_name2 != '':
+            file2 = 'mlbstats/static/player-stats/' + player_name2 + '_player_stats.csv'
+        stat_line_chart = create_chart(file1, stat_form, player_name1, file2, player_name2)
     context = {
         'player_name1':  player_name1,
         'line_chart': stat_line_chart,
         'navActivePlayer': ' active',
     }
     return render(request, 'player.html', context)
-
-def create_chart(stat_form, player_name1, x_labels, y_values):
-    line_chart = pygal.Bar()
-    line_chart.title = stat_form + ' per Season'
-    line_chart.x_labels = map(str, x_labels)
-    line_chart.add(player_name1, y_values)
-    stat_line_chart = line_chart.render_data_uri()
-    return stat_line_chart
 
 def generate_chart_values(file, stat_form):
     list_for_csv = []
@@ -128,9 +124,35 @@ def generate_chart_values(file, stat_form):
             for row in csv_reader:
                 x_labels.append(row[0])
                 y_values.append(float(row[idx]))
+    print('x_labels', x_labels)
     return x_labels, y_values
 
+def create_chart(file, stat_form, player_name1, file2=None, player_name2=None):
+    x_labels_1, y_values_1 = generate_chart_values(file, stat_form)
+    if file2:
+        x_labels_2, y_values_2 = generate_chart_values(file2, stat_form)
+        set_1 = set(x_labels_1)
+        set_2 = set(x_labels_2)
+        two_not_in_one = list(set_2 - set_1)
+        one_not_in_two = list(set_1 - set_2)
+        print('THE MISSING ONES:', one_not_in_two, two_not_in_one)
+        x_labels_merged = x_labels_1 + two_not_in_one
+        x_labels_merged.sort()
+        print('x_labels_final', x_labels_merged)
+    else:
+        x_labels_merged = x_labels_1
+    
 
+
+
+    line_chart = pygal.Bar()
+    line_chart.title = stat_form + ' per Season'
+    line_chart.x_labels = map(str, x_labels_merged)
+    line_chart.add(player_name1, y_values_1)
+    if file2:
+        line_chart.add(player_name2, y_values_2)
+    stat_line_chart = line_chart.render_data_uri()
+    return stat_line_chart
 
 def scorespast(request):
     print('--------view scorespast')
