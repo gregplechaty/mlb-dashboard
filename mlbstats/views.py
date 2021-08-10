cee3ca4fad2af355ff31909d45a8343ff330a9ba
Greg P <gplechaty@gmail.com>
2021-08-10 17:36:32 -0600

    two players with 0 y values for years they did not play

--- mlbstats/views.py
+++ mlbstats/views.py
@@ -93,7 +93,6 @@ def player(request):
     player_name2 = ''
     stat_line_chart = ''
     if 'player1' in request.GET:
-        print('---GET REQUEST:', request.GET)
         player_name1 = request.GET['player1']
         player_name2 = request.GET['player2']
         stat_form = request.GET['stat'].upper()
@@ -113,6 +112,12 @@ def generate_chart_values(file, stat_form):
     list_for_csv = []
     x_labels = []
     y_values = []
+
+    stats_dict = {}
+
+
+
+
     with open(file) as csv.file:
             csv_reader = csv.reader(csv.file, delimiter=',')
             header = next(csv_reader)
@@ -122,35 +127,43 @@ def generate_chart_values(file, stat_form):
                     break
             list_for_csv.append(csv_reader)
             for row in csv_reader:
+                stats_dict[row[0]] = float(row[idx])
                 x_labels.append(row[0])
                 y_values.append(float(row[idx]))
-    print('x_labels', x_labels)
-    return x_labels, y_values
+    #print('x_labels', x_labels)
+    #print('NEW DICT:', stats_dict)
+    return x_labels, y_values, stats_dict
 
 def create_chart(file, stat_form, player_name1, file2=None, player_name2=None):
-    x_labels_1, y_values_1 = generate_chart_values(file, stat_form)
+    x_labels_1, y_values_1, stats_dict_1 = generate_chart_values(file, stat_form)
     if file2:
-        x_labels_2, y_values_2 = generate_chart_values(file2, stat_form)
+        x_labels_2, y_values_2, stats_dict_2 = generate_chart_values(file2, stat_form)
         set_1 = set(x_labels_1)
         set_2 = set(x_labels_2)
         two_not_in_one = list(set_2 - set_1)
         one_not_in_two = list(set_1 - set_2)
-        print('THE MISSING ONES:', one_not_in_two, two_not_in_one)
         x_labels_merged = x_labels_1 + two_not_in_one
         x_labels_merged.sort()
-        print('x_labels_final', x_labels_merged)
+        for year in two_not_in_one:
+            stats_dict_1[year] = 0
+        for year in one_not_in_two:
+            stats_dict_2[year] = 0
+        y_labels_final_1 = []
+        y_labels_final_2 = []
+        for year in x_labels_merged:
+            y_labels_final_1.append(stats_dict_1[year])
+            y_labels_final_2.append(stats_dict_2[year])
     else:
         x_labels_merged = x_labels_1
-    
-
-
-
     line_chart = pygal.Bar()
     line_chart.title = stat_form + ' per Season'
     line_chart.x_labels = map(str, x_labels_merged)
-    line_chart.add(player_name1, y_values_1)
+    
     if file2:
-        line_chart.add(player_name2, y_values_2)
+        line_chart.add(player_name1, y_labels_final_1)
+        line_chart.add(player_name2, y_labels_final_2)
+    else:
+        line_chart.add(player_name1, y_values_1)
     stat_line_chart = line_chart.render_data_uri()
     return stat_line_chart
 
